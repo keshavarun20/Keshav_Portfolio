098211de8ccdc441a433082f32f451d00d05a214
keshavarun20 <keshavarun1220@gmail.com>
2024-09-15 16:16:51 +0530

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import About from "./sections/About"
 import Contact from "./sections/Contact"
 import Footer from "./sections/Footer"
 import Hero from "./sections/Hero"
-import Navbar from "./Sections/Navbar"
+import Navbar from "./sections/Navbar"
 import Projects from "./sections/Projects"
 
 
